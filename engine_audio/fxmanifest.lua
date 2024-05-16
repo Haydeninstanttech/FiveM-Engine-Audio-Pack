365e0ce7988e197711b689b3c60a01b8f5e0a858
CloudSparc <131487048+Haydeninstanttech@users.noreply.github.com>
2024-05-15 19:40:48 -0700

    Add files via upload

---
+++ engine_audio/fxmanifest.lua
@@ -0,0 +1,84 @@
+-- Copy the Data File Line below and replace the "file name here" with the new resource "soundname.dat", do the same with the Files section. Make sure to remove all the Dashes!! 
+--data_file 'AUDIO_GAMEDATA' 'audioconfig/soundnamehere.dat'
+--data_file 'AUDIO_SOUNDDATA' 'audioconfig/soundnamehere.dat'
+--data_file 'AUDIO_WAVEPACK' 'sfx/dlc_soundnamehere'
+--data_file 'AUDIO_GAMEDATA' 'audioconfig/soundnamehere.dat'
+--data_file 'AUDIO_SOUNDDATA' 'audioconfig/soundnamehere.dat'
+--data_file 'AUDIO_WAVEPACK' 'sfx/dlc_soundnamehere'
+
+--Sound Pack by CloudSparc
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/fordvoodoo_game.dat151.rel',
+  'audioconfig/fordvoodoo_sounds.dat54.rel',
+  'sfx/dlc_fordvoodoo/fordvoodoo.awc',
+  'sfx/dlc_fordvoodoo/fordvoodoo_npc.awc',
+  "audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc",
+  'audioconfig/r35sound_game.dat151.rel',
+	'audioconfig/r35sound_sounds.dat54.rel',
+	'sfx/dlc_r35sound/r35sound.awc',
+	'sfx/dlc_r35sound/r35sound_npc.awc'
+  
+
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/fordvoodoo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/fordvoodoo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fordvoodoo'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg57mustangtv8_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg57mustangtv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg57mustangtv8_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg57mustangtv8"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/r35sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta023l539_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta023l539_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta023l539'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/bgw16_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bgw16_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bgw16_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bgw16'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+data_file "AUDIO_GAMEDATA" "audioconfig/kc74s1000rrakevoline_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/kc74s1000rrakevoline_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_kc74s1000rrakevoline"
+data_file "AUDIO_SYNTHDATA" "audioconfig/szkgsxryoshimuraeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/szkgsxryoshimuraeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/szkgsxryoshimuraeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_szkgsxryoshimuraeng"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/53vortec_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/53vortec_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_53vortec'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/64powerstroke_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/64powerstroke_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_64powerstroke'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/46fordmodular_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/46fordmodular_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_46fordmodular'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/12vcummins_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/12vcummins_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_12vcummins'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/dodgehemihellcat_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/dodgehemihellcat_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_dodgehemihellcat'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/demonv8_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/demonv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/demonv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_demonv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tamustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tamustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tamustanggt50'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tascmustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tascmustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tascmustanggt50'
